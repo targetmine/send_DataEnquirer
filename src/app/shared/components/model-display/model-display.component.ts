a470aeca63b2af8331cc09a0bd4e02e3432275ea
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-04-10 17:02:47 +0900

    Empty display of tables for the current model.

--- src/app/shared/components/model-display/model-display.component.ts
+++ src/app/shared/components/model-display/model-display.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable, MatTableDataSource} from '@angular/material/table';
+
+import { Element } from '../../models/element';
+import { Relation } from '../../models/relation';
 
 @Component({
   selector: 'app-model-display',
   templateUrl: './model-display.component.html',
   styleUrls: ['./model-display.component.css']
 })
-export class ModelDisplayComponent {
+export class ModelDisplayComponent implements OnInit{
+	
+	elements: MatTableDataSource<Element> = new MatTableDataSource<Element>();
+	relations: MatTableDataSource<Relation> = new MatTableDataSource<Relation>();
 
+	@ViewChild('elementsTable')
+	elementTable!: MatTable<Element[]>;
+	elementTableColumns: string[] = ['elements', 'attributes'];
+
+	@ViewChild('relationsTable')
+	relationTable!: MatTable<Relation[]>;
+	relationTableColumns: string[] = ['source', 'target', 'cardinality'];
+
+
+	ngOnInit(): void {
+		throw new Error('Method not implemented.');
+	}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModelDisplayComponent } from './components/model-display/model-display.component';
 
-
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
 		ModelDisplayComponent
 	],
   imports: [
-    CommonModule
+    CommonModule,
+		MatTableModule
   ],
 	exports: [
 		ModelDisplayComponent
