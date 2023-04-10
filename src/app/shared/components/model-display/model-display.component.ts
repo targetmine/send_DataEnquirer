a7590b682b8745bb53c512c2ed8a3ed318991c1c
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-04-10 15:58:26 +0900

    Added empty model-display component.

---
+++ src/app/shared/components/model-display/model-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-display',
+  templateUrl: './model-display.component.html',
+  styleUrls: ['./model-display.component.css']
+})
+export class ModelDisplayComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModelDisplayComponent } from './components/model-display/model-display.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+		ModelDisplayComponent
+	],
   imports: [
     CommonModule
-  ]
+  ],
+	exports: [
+		ModelDisplayComponent
+	]
 })
 export class SharedModule { }
