42db26fa29d8d2ddd38e51e9f950a3107d227189
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-04-10 11:28:42 +0900

    Add modules for future shared elements and querier components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuerierModule } from './querier/querier.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+		HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		QuerierModule,
+		SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
