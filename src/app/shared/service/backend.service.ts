73b283488db6fba9ff539546a1721d9a9bc9470e
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-04-11 15:27:36 +0900

    Add empty backend service.

---
+++ src/app/shared/service/backend.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+
+  constructor(
+		private http: HttpClient
+	) { }
+}
