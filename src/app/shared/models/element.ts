8e258df0b9c0f371cf697f7510ec9a7eca7d5431
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-05-15 11:00:38 +0900

    Refactored all model related interfaces to single file.

--- src/app/shared/models/element.ts
+++
@@ -1,8 +0,0 @@
-import { Attribute } from './attribute';
-
-/** A basic element of a data model, it can be thought of as a table
- * in the context of a relational database */
-export interface Element{
-	name: string;
-	attributes: Attribute[];
-}
--- src/app/shared/models/model.ts
+++ src/app/shared/models/model.ts
@@ -1,9 +1,32 @@
-import { Element } from 'src/app/shared/models/element';
-import { Relation } from 'src/app/shared/models/relation';
-
-export interface Model {
+export interface Model{
 	elements: Element[],
 	relations: Relation[]
+}
+
+export interface Element{
+	name: string;
+	attributes: Attribute[];
+}
+
+export const ATTRIBUTE_TYPES = ['varchar(40)', 'text', 'integer', 'double precision'] as const;
+export type AttributeType = typeof ATTRIBUTE_TYPES[number];
+export interface Attribute{
+	name: string;
+	type: AttributeType;
+	unique: boolean;
+}
+
+export const CARDINALITY = ['one to one', 'one to many', 'many to many'] as const; 
+export type Cardinality = typeof CARDINALITY[number];
+export interface Relation{
+	name: string;
+	srcElement: string;
+	srcAttribute: string;
+	srcType: AttributeType;
+	trgElement: string;
+	trgAttribute: string;
+	trgType: AttributeType;
+	cardinality: Cardinality;
 }
 
 export interface ModelNode {
