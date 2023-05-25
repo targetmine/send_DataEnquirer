055464ddbe0478a7043464c8f17b2556f88b735e
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-05-25 12:52:16 +0900

    Added elements to handle relations to modelService.

--- src/app/shared/service/model.service.ts
+++ src/app/shared/service/model.service.ts
@@ -2,7 +2,7 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-import { ModelNode } from 'src/app/shared/models/model';
+import { Model, ModelNode } from 'src/app/shared/models/model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +11,25 @@ export class ModelService {
 
 	elements$: BehaviorSubject<ModelNode[]>;
 	selectedElements$: SelectionModel<ModelNode>;
+
+	relations$: BehaviorSubject<ModelNode[]>;
+	selectedRelations$: SelectionModel<ModelNode>;
 	
   constructor() { 
 		this.elements$ = new BehaviorSubject<ModelNode[]>([]);
 		this.selectedElements$ = new SelectionModel<ModelNode>(true);
+
+		this.relations$ = new BehaviorSubject<ModelNode[]>([]);
+		this.selectedRelations$ = new SelectionModel<ModelNode>(true);
 	}
 
 	getSelectedElements(){
 		const root = this.elements$.value[0];
 		return root.children.filter(_ => this.selectedElements$.isSelected(_));
+	}
+
+	getSelectedRelations(){
+		const root = this.relations$.value[0];
+		return root.children.filter(_ => this.selectedRelations$.isSelected(_));
 	}
 }
