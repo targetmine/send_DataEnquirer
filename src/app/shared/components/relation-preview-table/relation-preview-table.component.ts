04be796c24339aafe9688f72e0e5cecb09859a1f
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-05-30 16:45:52 +0900

    Definition of table and attributes to retrieve for relations data (tested only for 1to1).

--- src/app/shared/components/relation-preview-table/relation-preview-table.component.ts
+++ src/app/shared/components/relation-preview-table/relation-preview-table.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { BackendService } from '../../service/backend.service';
 import { ModelService } from '../../service/model.service';
-import { ModelNode } from 'src/app/shared/models/model';
+import { Cardinality, ModelNode } from 'src/app/shared/models/model';
+import { Element } from 'src/app/shared/models/model';
+import { MatTab } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-relation-preview-table',
   templateUrl: './relation-preview-table.component.html',
   styleUrls: ['./relation-preview-table.component.css']
 })
-export class RelationPreviewTableComponent implements OnInit {
+export class RelationPreviewTableComponent{
+	elements!: Element[];
 	@Input() relation!: ModelNode;
 
 	@ViewChild('previewTable')
@@ -20,17 +23,53 @@ export class RelationPreviewTableComponent implements OnInit {
 	constructor(
 		public readonly backendService: BackendService,
 		public readonly modelService: ModelService
-	){ }
-
-	ngOnInit(): void {
-			
+	){ 
+		this.backendService.getModel()
+		.then(response => {
+			this.elements = response.body?.elements ? 
+				response.body.elements :
+				[] as Element[];
+		})
+		.catch(error => {
+			console.error(`Error loading the model from the server: ${error}`);
+			this.elements = [] as Element[];
+		});
 	}
 
 	onPreview(){
-
+		const [source, target, cardinality] = this.relation.name.split(' - ');
+		const [srcEle, srcAttr] = source.split('/');
+		const [trgEle, trgAttr] = target.split('/');
+		let name: string;
+		let columns: string[];
+		switch(cardinality){
+			case 'one to one':
+				name = srcEle;
+				columns = [srcAttr, target.replace('/','_')];
+				break;
+			case 'one to many':
+				name = trgEle;
+				columns = [source.replace('/', '_'), trgAttr];
+				break;
+			default: // 'many to many'
+				name = `${source.split('/')[0].trim()}_${target.split('/')[0].trim()}`;
+				columns = ['*'];
+				break;
+		 }
+		this.backendService.getElement(name, columns, 10)
+		.then(response => {
+			const data = response.body ?
+				response.body as any[] :
+				[] as any[];
+			// NEED to parse the body into an array of {target source} elements
+			this.previewTableData = new MatTableDataSource(data);
+		})
+		.catch(error => {
+			console.error(error);
+		});
 	}
 
 	onDownload(){
-		
+
 	}
 }
