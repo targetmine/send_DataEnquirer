81d2b22211a832b5f9e1bfe566a3b54c847cc22b
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-05-31 16:27:33 +0900

    Add download data  option for relations.

--- src/app/shared/components/relation-preview-table/relation-preview-table.component.ts
+++ src/app/shared/components/relation-preview-table/relation-preview-table.component.ts
@@ -75,6 +75,46 @@ export class RelationPreviewTableComponent{
 	}
 
 	onDownload(){
-
+		const [source, target, cardinality] = this.relation.name.split(' - ');
+		const [srcEle, srcAttr] = source.split('/');
+		const [trgEle, trgAttr] = target.split('/');
+		let name: string;
+		let columns: string[];
+		switch(cardinality){
+			case 'one to one':
+				name = srcEle;
+				columns = [srcAttr, target.replace('/','_')];
+				break;
+			case 'one to many':
+				name = trgEle;
+				columns = [source.replace('/', '_'), trgAttr];
+				break;
+			default: // 'many to many'
+				name = `${source.split('/')[0].trim()}_${target.split('/')[0].trim()}`;
+				columns = ['*'];
+				break;
+		 }
+		this.backendService.getElement(name, columns)
+		.then(response => {
+			let data = response.body as any[]; 
+			if( data.length === 0 ){
+				console.log('No data to download');
+				return;
+			}
+			const header = Object.keys(data[0]);
+			const replacer = (key: any, value: any) => value === null ? '' : value; 
+			let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+			csv.unshift(header.join(','));
+			const csvArray = csv.join('\r\n');
+			let objectUrl = URL.createObjectURL(new Blob([csvArray], {type: "text/csv"}));
+			const a = document.createElement('a');
+			a.download = `data.csv`;
+			a.href = objectUrl;
+			a.click();
+			URL.revokeObjectURL(objectUrl);	
+		})
+		.catch(error => {
+			console.error(error);
+		});
 	}
 }
